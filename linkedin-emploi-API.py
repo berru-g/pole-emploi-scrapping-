3c10c1e9c28571edc6204d4ea5c061b4cbb6c902
berru-g <g.leberruyer@gmail.com>
2023-03-14 17:34:47 +0100

    Step 9

--- linkedin-emploi-API.py
+++ linkedin-emploi-API.py
@@ -18,3 +18,17 @@ for job in jobs:
     # Envoyer une réponse automatique avec votre site Web
     message = f"Bonjour {job['companyName']}, je suis intéressé par le poste de {job['title']}. Pour en savoir plus sur mes compétences et mon expérience, veuillez consulter mon site Web {votre lien de site Web ici}"
     api.send_message_v2(recipient=job['listedJobPoster']['miniProfile'], message=message)
+    
+    
+# récupérer toutes les conversations liées à votre compte LinkedIn
+conversations = api.get_conversations()
+
+# filtrer les conversations avec les recruteurs
+recruiter_conversations = [conv for conv in conversations if conv['entityUrn'].startswith('urn:li:fs_conversation:') and conv['participants'][0]['firstName'].lower() == 'recruiter']
+
+# extraire les liens des annonces d'emploi correspondantes
+for conv in recruiter_conversations:
+    messages = api.get_conversation_messages_v2(conv['entityUrn'])
+    for message in messages:
+        if 'jobPostingUrl' in message:
+            print(message['jobPostingUrl'])
