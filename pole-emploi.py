23b54cabe16a6f308fa354ae87d3892f0bbeca50
berru-g <g.leberruyer@gmail.com>
2023-03-14 18:30:50 +0100

    lien

--- pole-emploi.py
+++ pole-emploi.py
@@ -4,8 +4,9 @@ from bs4 import BeautifulSoup
 # Send an HTTP request to the Pole Emploi job search page
 url = "https://candidat.pole-emploi.fr/offres/recherche" # URL de la page de recherche
 params = {'offresPartenaires': 'true', 'range': '0-9', 'rayon': '10', 'tri': '0', 'motsCles': 'dev front-end développeur web', 'lieux': '44'} 
-headers = {'User-Agent': 'Mozilla/5.0'}
-response = requests.get(url, headers=headers, params=params)
+#headers = {'User-Agent': 'Mozilla/5.0'}
+#response = requests.get(url, headers=headers, params=params)
+response = requests.get(url)
 
 # Parse the HTML content
 soup = BeautifulSoup(response.content, "html.parser")
