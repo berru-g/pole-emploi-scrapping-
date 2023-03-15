9b832501ebfc027f4ac4b0d5506a8ff1009b3bb5
berru-g <g.leberruyer@gmail.com>
2023-03-15 16:58:39 +0100

    more class get

--- script/pole-emploi.py
+++ script/pole-emploi.py
@@ -1,14 +1,17 @@
 """
 Pratiquer cela dans des environnements de test pour éviter de bloquer votre ip.
 from fake_useragent import UserAgent
-
+STEP 1
+change lieux={} &motscles={} et trouve toute les annonces
+STEP 2
+envoie mail auto
 """
 from pyautogui import sleep 
 import requests
 from bs4 import BeautifulSoup
 #STEP 1
-# Send an HTTP request to the website
-url = "https://candidat.pole-emploi.fr/offres/recherche?lieux=44109&motsCles=fullstack&offresPartenaires=true&rayon=10&tri=0" #caution: last url is 
+# Send an HTTP request to the website // change lieux={} &motscles={}
+url = "https://candidat.pole-emploi.fr/offres/recherche?lieux={44109}&motsCles={serveur}&offresPartenaires=true&rayon=10&tri=0" #caution: last url is 
 response = requests.get(url)
 
 # Parse the HTML content
@@ -31,7 +34,7 @@ print("Fin des offres")
     
     
 """
-#STEP 2
+#STEP 3
 send_url = "https://candidat.pole-emploi.fr/offres/emploi/developpeur-web/nantes/s29m2v6" #caution: last url is 
 offer_response = requests.get(send_url)
 # Find the link to the offer page
